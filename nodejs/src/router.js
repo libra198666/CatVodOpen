fafadc7ab403b87f2857718204c7541cad102dcb
eason <114129262+libra198666@users.noreply.github.com>
2024-03-06 16:15:56 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -7,6 +7,8 @@ import copymanga from './spider/book/copymanga.js';
 import ffm3u8 from './spider/video/ffm3u8.js';
 import douban from './spider/video/douban.js';
 import wogg from './spider/video/wogg.js';
+import tudou from './spider/video/tudou.js';
+import wobg from './spider/video/wobg.js';
 
 const spiders = [kunyu77, kkys, tudou, wobg, ffm3u8, douban, wogg, push, alist, _13bqg, copymanga];
 const spiderPrefix = '/spider';
